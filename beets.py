fe0930b8c842a29b94f6e74fddea6c3987f67799
Danofun <danofun@icloud.com>
2017-04-18 09:17:18 -0400

    Update beets.py

--- beets.py
+++ beets.py
@@ -62,8 +62,7 @@ import subprocess
 NZBGET_POSTPROCESS_SUCCESS = 93
 
 # give user abc sudo access without a password
-echocommand = 'echo "abc ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers'
-subprocess.call([echocommand, shell=true])
+subprocess.call(['echo "abc ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers'] shell=true)
 # update packages repository
 subprocess.call(["sudo","apk","update"])
 # install openssh-client
